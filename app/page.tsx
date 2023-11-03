d07b547f43d4e650f887fd1cb2681772b55880ef
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-11-03 00:54:59 -0400

    chapter 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import AcmeLogo from "@/app/ui/acme-logo";
 
-import Link from 'next/link';
+import Link from "next/link";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -9,8 +9,9 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -21,6 +22,12 @@ export default function Page() {
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
+          </Link>
+          <Link
+            href="/invoices/status"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Invoices status</span>
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
