ec50e0b65cc226a6d197bae3bc88bac09837260b
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-11-03 01:19:41 -0400

    routing

--- app/page.tsx
+++ app/page.tsx
@@ -24,10 +24,10 @@ export default function Page() {
             <span>Log in</span>
           </Link>
           <Link
-            href="/invoices/status"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Invoices status</span>
+            <span>Dashboard</span>
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
